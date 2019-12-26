1246297bfdacbc13660635c21cdcfd5156947fd6
polina090193 <52452187+polina090193@users.noreply.github.com>
2019-12-26 19:49:47 +0300

    Add new landscape styles to file

--- fullpageOptions.js
+++ fullpageOptions.js
@@ -4,7 +4,6 @@ new fullpage('.fullscreen-slide-wrapper', {
     fixedElements: '.menu__mobile',
     css3: false,
     controlArrows: false,
-    normalScrollElements: '.classes-description',
 });
 
 //methods
